4dc2f283bb400d0d21520b9c66e602ba8282bde5
Eva Čechová <cechovaev@gmail.com>
2024-12-02 20:46:39 +0100

    changed calendar.tsx according to chatGPT to fix the unused props issue

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,11 +1,13 @@
-import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import { DayPicker } from "react-day-picker";
+import { DayPicker, DayPickerProps } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 
-export type CalendarProps = React.ComponentProps<typeof DayPicker>;
+export type CalendarProps = DayPickerProps & {
+	className?: string;
+	classNames?: Partial<DayPickerProps["classNames"]>;
+};
 
 function Calendar({ className, classNames, showOutsideDays = true, ...props }: CalendarProps) {
 	return (
@@ -51,13 +53,14 @@ function Calendar({ className, classNames, showOutsideDays = true, ...props }: C
 				...classNames,
 			}}
 			components={{
-				IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-				IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+				IconLeft: (iconProps) => <ChevronLeft {...iconProps} className="h-4 w-4" />,
+				IconRight: (iconProps) => <ChevronRight {...iconProps} className="h-4 w-4" />,
 			}}
 			{...props}
 		/>
 	);
 }
+
 Calendar.displayName = "Calendar";
 
 export { Calendar };
