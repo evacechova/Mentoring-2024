7d36ad8a4b1d340fee2198fbeac465c4618ea67c
Eva Čechová <cechovaev@gmail.com>
2024-12-22 14:00:03 +0100

    established connection between Lambda function and React front-end, fixed an issue with innactive CORS and APIkey

--- src/calendar-api.js
+++ src/calendar-api.js
@@ -1,10 +1,13 @@
 async function fetchGoogleCalendarEvents() {
+    const apiKey = "XhojsfxUPY3W2osq01mb59lOKGW8rjXO9K2w26C1";
     const apiUrl = "https://ghvy6isiq6.execute-api.eu-north-1.amazonaws.com/dev/events";
     try {
-        const response = await fetch(apiUrl);
+        const response = await fetch(apiUrl, { method: "GET", headers: { "x-api-key": apiKey } });
         const data = await response.json();
         return data; // Google Calendar events
     } catch (error) {
         console.error("Error fetching events:", error);
     }
 }
+
+console.log(fetchGoogleCalendarEvents());
