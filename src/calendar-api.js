3cfed023e067f492d4b3c178a8553651b7c9be14
Eva Čechová <cechovaev@gmail.com>
2024-12-26 00:35:47 +0100

    env testing

--- src/calendar-api.js
+++ src/calendar-api.js
@@ -1,15 +1,11 @@
-require('dotenv').config();
-
-async function fetchGoogleCalendarEvents() {
-    const apiKey = process.env.REACT_APP_CALENDAR_API_KEY;
-    const apiUrl = "https://ghvy6isiq6.execute-api.eu-north-1.amazonaws.com/dev/events";
-    try {
-        const response = await fetch(apiUrl, { method: "GET", headers: { "x-api-key": apiKey } });
-        const data = await response.json();
-        return data; // Google Calendar events
-    } catch (error) {
-        console.error("Error fetching events:", error);
-    }
+export const fetchGoogleCalendarEvents = async () => {
+	const apiKey = import.meta.env.VITE_AWS_API_KEY;
+	const apiUrl = "https://ghvy6isiq6.execute-api.eu-north-1.amazonaws.com/dev/events";
+	try {
+		const response = await fetch(apiUrl, { method: "GET", headers: { "x-api-key": apiKey } });
+		const data = await response.json();
+		return data; // Google Calendar events
+	} catch (error) {
+		console.error("Error fetching events:", error);
+	}
 }
-
-console.log(fetchGoogleCalendarEvents());
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -7,20 +7,7 @@ import { CTO } from "../components/CTO/CTO";
 import { Calendar } from "@/components/ui/calendar";
 
 /* for test function calling*/
-async function fetchGoogleCalendarEvents() {
-	const apiKey = "XhojsfxUPY3W2osq01mb59lOKGW8rjXO9K2w26C1";
-	const apiUrl = "https://ghvy6isiq6.execute-api.eu-north-1.amazonaws.com/dev/events";
-	try {
-		const response = await fetch(apiUrl, { method: "GET", headers: { "x-api-key": apiKey } });
-		const data = await response.json();
-		console.log(data.body);
-		return data; // Google Calendar events
-	} catch (error) {
-		console.error("Error fetching events:", error);
-	}
-}
-
-console.log(fetchGoogleCalendarEvents());
+// console.log(fetchGoogleCalendarEvents());
 
 export const Homepage = () => {
 	const [date, setDate] = useState<Date | undefined>(new Date());
