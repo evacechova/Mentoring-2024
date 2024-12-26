69fa787a1702882c9e4da258a28bdf18c9207e93
Eva Čechová <cechovaev@gmail.com>
2024-12-26 18:50:10 +0100

    testing commit

--- src/calendar-api.ts
+++ src/calendar-api.ts
@@ -1,10 +1,18 @@
 export const fetchGoogleCalendarEvents = async () => {
 	const apiKey = import.meta.env.VITE_AWS_API_KEY;
 	const apiUrl = "https://ghvy6isiq6.execute-api.eu-north-1.amazonaws.com/dev/events";
+
 	try {
 		const response = await fetch(apiUrl, { method: "GET", headers: { "x-api-key": apiKey } });
 		const data = await response.json();
-		return data; // Google Calendar events
+		const events = JSON.parse(data.body);
+
+		const processedEvents = events.map((event) => ({
+			startDate: event.start?.dateTime || event.start?.date, // Handle all-day events
+			endDate: event.end?.dateTime || event.end?.date, // Handle all-day events
+		}));
+		return processedEvents; // Google Calendar events
+		// return data; // Google Calendar events
 	} catch (error) {
 		console.error("Error fetching events:", error);
 	}
