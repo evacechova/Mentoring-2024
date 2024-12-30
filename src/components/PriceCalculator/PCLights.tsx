aab2b771cc4bead4336821f6487b9366c4954e8f
Eva Čechová <cechovaev@gmail.com>
2024-12-30 13:45:37 +0100

    updated checkbox to allow unchecking

--- src/components/PriceCalculator/PCLights.tsx
+++ src/components/PriceCalculator/PCLights.tsx
@@ -1,11 +1,15 @@
-export const PCLights: React.FC<PCLightsProps> = ({
-	lightsCost,
-	setLightsCost,
-}: {
+interface PCLightsProps {
 	lightsCost: number;
 	setLightsCost: (value: number) => void;
-}) => {
+}
+
+export const PCLights: React.FC<PCLightsProps> = ({ lightsCost, setLightsCost }) => {
 	const option = { label: "With lights", value: 100 };
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const isChecked = e.target.checked;
+		setLightsCost(isChecked ? option.value : 0); //updating the state based on whether the checkbox is checked
+	};
 
 	return (
 		<div className="pclights">
@@ -16,7 +20,8 @@ export const PCLights: React.FC<PCLightsProps> = ({
 				id={`pclights${option.label}`}
 				value={option.value}
 				checked={lightsCost === option.value}
-				onChange={(e) => setLightsCost(Number(e.target.value))}
+				// onChange={(e) => setLightsCost(Number(e.target.value))}
+				onChange={handleChange}
 			/>
 		</div>
 	);
