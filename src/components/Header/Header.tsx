351533a989118b29b77925bc104464f3d363fff2
Eva Čechová <cechovaev@gmail.com>
2025-01-05 15:18:09 +0100

    added dynamic rendering of header menu items

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,23 @@
 import { Link } from "react-router-dom";
+import { ROUTES } from "@/globalVariables";
 
 export const Header = () => {
 	return (
 		<div>
-			<Link to="/">
-				<h1>Amateur at Everything</h1>
-			</Link>
+			<div>
+				<Link to="/">
+					<h1>Amateur at Everything</h1>
+				</Link>
+			</div>
+			<div>
+				<ul>
+					{Object.entries(ROUTES).map(([key, { title, route }], _index) => (
+						<li key={key}>
+							<Link to={route}>{title}</Link>
+						</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 };
